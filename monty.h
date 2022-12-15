b8ce38ba5222fd722252fba78d52045645fc7425
youssefbelfeki <youssef.belfeki@gmail.com>
2022-12-15 12:53:45 -0800

    pint.c

--- monty.h
+++ monty.h
@@ -2,9 +2,9 @@
 #define MONTY_H
 #include <stddef.h>
 #include <stdlib.h>
-#define INSTRUCTIONS              \
-{                           \
-	{"push", push},       \
+#define INSTRUCTIONS \
+        {                           \
+		{"push", push},       \
 	{"pall", pall},   \
 	{"pint", pint},   \
 	{"pop", pop},     \
